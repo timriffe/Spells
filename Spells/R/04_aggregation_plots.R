ea554db267fd9d091e9c47ef804df69405d490b6
Tim Riffe <tim.riffe@gmail.com>
2020-02-21 14:38:24 +0100

    added geom_ribbon, changed x range for means

--- Spells/R/04_aggregation_plots.R
+++ Spells/R/04_aggregation_plots.R
@@ -2,10 +2,10 @@ library(here); library(devtools); library(TraMineR); library(tidyverse); library
 library(descr); library(survey); library(RColorBrewer)
 remove(list=ls())
 
-load(here('Spells', 'Data', 'Castro', 'cas_wom_dhs_raw_senegal.RData'))
+load(here('Spells', 'Data', 'Castro', 'cas_wom_dhs_raw_colombia.RData'))
 dr<-db[, c('ident', 'filenw','v005','v001')]
 
-df<-data.frame(readRDS(here("Spells","Data","Castro","cas_wom_senegal.rds")))
+df<-data.frame(readRDS(here("Spells","Data","Castro","cas_wom_colombia.rds")))
 #db<-readRDS(here("Spells","Data","Castro","cas_wom_colombia.rds"))
 
 
@@ -25,28 +25,65 @@ sd<-svydesign(~filenw.y+v001.y, weights=~pwt, data=db); sd
 
 # Estimates for the next birth
 est_nc<-svyby(~c_step_down_par1, ~left_par1+afb5+sexf, 
-           subset(sd, left_par1>=0 & left_par1<=10), 
+           subset(sd, left_par1>=0), 
            FUN=svymean, na.rm=T)
 summary(est_nc)
 est_nc[1:22,]
 
 # Estimates for the next birth
 est_nb<-svyby(~c_step_down_boy1, ~left_par1+afb5+sexf, 
-              subset(sd, left_par1>=0 & left_par1<=10), 
+              subset(sd, left_par1>=0), 
               FUN=svymean, na.rm=T)
 summary(est_nb)
 est_nb[1:22,]
 
 # Estimates for the next birth
 est_ng<-svyby(~c_step_down_gir1, ~left_par1+afb5+sexf, 
-              subset(sd, left_par1>=0 & left_par1<=10), 
+              subset(sd, left_par1>=0), 
               FUN=svymean, na.rm=T)
 summary(est_ng)
 est_ng[1:22,]
 Sys.time()
 
+# for sake of easier plotting, change colnames of x variable to be identical
+
+est_nc <- 
+  est_nc %>% 
+  rename(y = c_step_down_par1)
+est_nb <- 
+  est_nb %>% 
+  rename(y = c_step_down_boy1)
+est_ng <- 
+  est_ng %>% 
+  rename(y = c_step_down_gir1)
 
 setwd(here("Spells","Figures"))
+
+est_nb
+est_nb %>% 
+  mutate(sexf = as.factor(sexf)) %>% 
+ggplot(mapping = aes(x = left_par1, 
+                     y = y, 
+                     color = sexf)) + 
+  geom_line() + 
+  geom_ribbon(mapping=aes(ymin = y - 2*se, ymax = y + 2 * se, 
+                          fill = sexf),alpha=.3, colour = NA)+
+  #geom_vline(xintercept = c(0,1)) +
+  # transparent band presumed to isolate twins
+  annotate("rect", xmin = 0, xmax = 1, ymin = 0, ymax = 5, alpha = .2) +
+  facet_wrap(~afb5) + 
+  xlim(0,10)+ ylim(0,5) + 
+  scale_color_manual(labels = c("Boy", "Girl"), values = col7[c(2,5)]) +
+  scale_fill_manual(labels = c("Boy", "Girl"), values = col7[c(2,5)]) +
+  guides(color=guide_legend(title="First child")) + 
+  xlab("Time since first birth") + 
+  ylab("Mean time to second birth")
+  
+  
+  
+dev.print(device=pdf, paste0("mt_birth2_", names(analysis)[c], ".pdf"), width=7, height=5)
+
+
 
 # left first BIRTH time left to second birth, stratified by
 # sex of the first birth AND age at first birth
