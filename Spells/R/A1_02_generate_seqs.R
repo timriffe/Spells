f51c563d30c86936353dee70a89513b6859da65e
Tim Riffe <tim.riffe@gmail.com>
2020-03-09 23:26:17 +0100

    boot code left running overnight

--- Spells/R/A1_02_generate_seqs.R
+++ Spells/R/A1_02_generate_seqs.R
@@ -11,106 +11,43 @@ library(tidyverse)
 library(markovchain)
 library(reshape2)
 library(stringr)
+library(rlang)
 #library(Spells)
 library(devtools)
+library(data.table)
 # 0) load functions
 source(here::here("Spells","R","LorentiFunctions.R"))
 
 load_all(here("Spells","R","Spells"))
 TR <- readRDS(here("Spells","Data","Lorenti","boot_females_tp_limitations.rda"))
-TR %>% 
-  mutate(from =  str_extract(from,"[a-z,A-Z]+"),
-         sex = "F") %>% 
-  rename(state_from = from) %>% 
+
+TR <- TR %>% 
   # just pick out extreme quintiles
   filter(InQ %in% c("I","V")) %>% 
-  group_by(InQ)
-  
+  mutate(from = as.character(from),
+         from =  str_extract(from,"[a-z,A-Z]+"),
+         state_to = as.character(state_to),
+         sex = "F") %>% 
+  rename(state_from = from) 
 
-# This should be called inside a 'do()'?
-get_trajectories <- function(
-  X, 
-  .InQ = "I", 
-  .sex = "F", 
-  Ntraj = 50000, 
-  case = 1){
-  Fi <- getU(X, sex = .sex, InQ = .InQ) 
-  Fimc <- closeout(Fi)
-  # make sim matrix
-  Fsim  <- replicate(Ntraj,
-                     rmarkovchain(n = 65, 
-                                  object = FVmc, 
-                                  t0 = "16::Healthy", 
-                                  parallel = TRUE)
-                     )
-                  
-  dimnames(Fsim = list(16:80, 1:Ntraj))
-  Fsim           <- gsub(".*:","", Fsim)
-  # start pipe
-  Fsim <-
-    Fsim %>% 
-   melt(varnames = c("age","id"), 
-        value.name = "state") %>% 
-   mutate(InQ = .InQ, sex = .sex) %>% 
-   filter(age < 80) %>% # it's closed out so everyone dead at 80...
-   group_by(InQ, id) %>% 
-   mutate(dead = ifelse(any(state == "Dead"),TRUE,FALSE)) %>% 
-   ungroup() %>% 
-   filter(state != "Dead") %>% 
-   group_by(InQ, id) %>% 
-    # say we want average duration of disability
-    # * spells starting in age x
-    # * spells ending in age x
-    # avg duration of nth spells by age.
-   mutate(dis_dur = clock(state, 
-                          state = "Disabled",
-                          clock_type = "duration"),
-          dis_order = clock(state, 
-                            state = "Disabled",
-                            clock_type = "order",
-                            increasing = TRUE)) %>% 
-   group_by(InQ, id, dis_order) %>% 
-   mutate(first = row_number() == 1L,
-          last = row_number() == n(),
-          first = ifelse(state == "Disabled" & 
-                         max(age) == 79, FALSE, first),
-          last = ifelse(state == "Disabled" & 
-                           max(age) == 79, FALSE, last)) 
-  
-  # now toggle output depending on application:
-  if (case == 1){
-    Fsim <- Fsim %>% 
-      ungroup() %>% 
-      mutate(InQ = case_when(InQ == "I" ~ "lowest 20%",
-                             InQ == "V" ~ "highest 20%")) %>% 
-      filter(first,
-             !is.na(dis_dur)) %>% 
-      group_by(sex, InQ, age) %>% 
-      summarize(dur_first_mean = mean(dis_dur, na.rm = TRUE)) %>% 
-      mutate(dur_first_mean = na_if(dur_first_mean, NaN))
-    # ready to plot!
-  }
-  
-  if (case == 2){
-    Fsim <- Fsim %>% 
-      filter(first,
-             !is.na(dis_dur)) %>% 
-      group_by(sex, InQ, age) %>% 
-      summarize(order_first_mean = mean(dis_order, na.rm = TRUE)) %>% 
-      mutate(order_first_mean = na_if(order_first_mean, NaN))
-  }
-  
-  if (case == 3){
-    Fsim <- Fsim %>%
-      filter(dead) %>% 
-      group_by(InQ, id) %>% 
-      mutate(ttd = max(age) - age,
-             ad5 = max(age) - max(age) %% 5) %>% 
-      ungroup() %>% 
-      filter(ad5 > 30) %>% 
-      group_by(sex, InQ, ttd, ad5) %>% 
-      summarize(ttdprev = mean(state == "Disabled"))
-  }
-  Fsim
-}
+
+TR <- as.data.table(TR)
+
+# Takes a looooong time
+A1.1 <- TR[, get_trajectories(.SD, Ntraj = 50000, case = 1), by = list(i,InQ,sex)]
+save.rds(A1.1, file = here::here("Spells","Data","Lorenti","A1.1.rds"))
+rm(A1.1);gc()
+
+A1.2 <- TR[, get_trajectories(.SD, Ntraj = 50000, case = 2), by = list(i,InQ,sex)]
+save.rds(A1.2, file = here::here("Spells","Data","Lorenti","A1.2.rds"))
+rm(A1.2);gc()
+
+A1.3 <- TR[, get_trajectories(.SD, Ntraj = 50000, case = 3), by = list(i,InQ,sex)]
+save.rds(A1.3, file = here::here("Spells","Data","Lorenti","A1.3.rds"))
+rm(A1.3);gc()
+
+rm(TR);gc()
+
+
+
 
--- Spells/R/LorentiFunctions.R
+++ Spells/R/LorentiFunctions.R
@@ -14,7 +14,7 @@ pi2u <- function(pivec,
   # to technically our first one goes from 48 to 50, not from 50 to 52.
   ages          <- ((0:n) * interval) + start_age - interval
   from_names    <- c(paste(from,ages[-length(ages)],sep="::"),"D::Inf")
-  to_names      <-c(paste(to,ages[-1],sep="::"),"D::Inf")
+  to_names      <- c(paste(to,ages[-1],sep="::"),"D::Inf")
   dimnames(out) <- list(to_names, from_names)
   out
 }
@@ -35,27 +35,32 @@ U2N <- function(U, interval = 2) {
 }
 
 # 2) function to get sub U for a given sex and quintile.
-getpiu <- function(TR, sexx = "M", InQx = "I", from = "Disabled", to = "Healthy"){
-  pivec <- TR %>% 
-    filter(sex == {{sexx}} &
-             InQ == {{InQx}} &
+getpiu <- function(TR, from = "Disabled", to = "Healthy", start_age = 15){
+  TR %>% 
+    # removed because group already selected in pipeline / data.table by statement
+    # filter(sex == {{sexx}} &
+    #          InQ == {{InQx}} &
+    filter(
              state_from == {{from}} &
              state_to == {{to}}) %>% 
-    pull(probs)
-  
-  pi2u(pivec, from = from, to = to, interval = 1, start_age = 16)
+    pull(probs) %>% 
+    pi2u(from = from, to = to, interval = 1, start_age = start_age)
 }
 
 # function to compose whole U for given sex and quintile
-getU <- function(TR, sexx = "M", InQx = "I"){
-  HH <- getpiu(TR = TR, sexx = sexx, InQx = InQx, 
-               from = "Healthy", to = "Healthy")
-  HU <- getpiu(TR = TR, sexx = sexx, InQx = InQx, 
-               from = "Healthy", to = "Disabled")
-  UH <- getpiu(TR = TR,sexx = sexx, InQx = InQx, 
-               from = "Disabled", to = "Healthy")
-  UU <- getpiu(TR = TR, sexx = sexx, InQx = InQx, 
-               from = "Disabled", to = "Disabled")
+getU <- function(TR, start_age = 15){
+  HH <- getpiu(TR = TR,
+               from = "Healthy", to = "Healthy", 
+               start_age = start_age)
+  HU <- getpiu(TR = TR, 
+               from = "Healthy", to = "Disabled", 
+               start_age = start_age)
+  UH <- getpiu(TR = TR, 
+               from = "Disabled", to = "Healthy",
+               start_age = start_age)
+  UU <- getpiu(TR = TR,
+               from = "Disabled", to = "Disabled",
+               start_age = start_age)
   U <- u2U(HH = HH, # healthy to healthy
            HU = HU, # healthy to Disabled
            UH = UH, # Disabled to healthy
@@ -65,7 +70,7 @@ getU <- function(TR, sexx = "M", InQx = "I"){
 
 # turn into usable markov objects:
 
-closeout <- function(U, name = "FV"){
+closeout <- function(U, name = "FV", start_age = 15){
   U[U < 0] <- 0
   U1 <- cbind(U, 0)
   U2 <- rbind(U1, 1 - colSums(U1))
@@ -75,7 +80,8 @@ closeout <- function(U, name = "FV"){
   U3 <- t(U2)
   
   # give adequate names
-  age_state   <- c(outer(seq(16,80,by=1),
+  # first data version started at age 16, later at 15?
+  age_state   <- c(outer(start_age:80,
                          paste0("::",c("Healthy","Disabled")),paste0),"Dead")
   
   dimnames(U3) <- list(to=age_state, from=age_state)
@@ -86,3 +92,94 @@ closeout <- function(U, name = "FV"){
       transitionMatrix = U3,
       name = name)
 }
+
+
+# Ugly beast function to be called inside do() for boot results..
+# This should be called inside a 'do()'?
+get_trajectories <- function(
+  X, 
+  Ntraj = 50000, 
+  case = 1){
+  
+  Fimc <- X %>% 
+    getU(start_age = 15) %>% 
+    closeout()
+  # make sim matrix
+  Fsim  <- replicate(Ntraj,
+                     rmarkovchain(n = 65, 
+                                  object = Fimc, 
+                                  t0 = "15::Healthy", 
+                                  parallel = TRUE)
+  )
+  
+  dimnames(Fsim) <- list(15:79, 1:Ntraj)
+  Fsim           <- gsub(".*:","", Fsim)
+  # start pipe
+  Fsim <-
+    Fsim %>% 
+    reshape2::melt(varnames = c("age","id"), 
+         value.name = "state") %>% 
+    mutate(state = as.character(state)) %>% 
+    filter(age < 80) %>% # it's closed out so everyone dead at 80...
+    group_by(id) %>% 
+    mutate(dead = ifelse(any(state == "Dead"),TRUE,FALSE)) %>% 
+    ungroup() %>% 
+    filter(state != "Dead") %>% 
+    group_by(id) %>% 
+    # say we want average duration of disability
+    # * spells starting in age x
+    # * spells ending in age x
+    # avg duration of nth spells by age.
+    mutate(dis_dur = clock(state, 
+                           state = "Disabled",
+                           clock_type = "duration"),
+           dis_order = clock(state, 
+                             state = "Disabled",
+                             clock_type = "order",
+                             increasing = TRUE)) %>% 
+    group_by(id, dis_order) %>% 
+    mutate(first = row_number() == 1L,
+           last = row_number() == n(),
+           first = ifelse(state == "Disabled" & 
+                            max(age) == 79, FALSE, first),
+           last = ifelse(state == "Disabled" & 
+                           max(age) == 79, FALSE, last)) 
+  
+  # now toggle output depending on application:
+  if (case == 1){
+    Fsim <- Fsim %>% 
+      ungroup() %>% 
+      filter(first,
+             !is.na(dis_dur)) %>% 
+      group_by(age) %>% 
+      summarize(dur_first_mean = mean(dis_dur, na.rm = TRUE)) %>% 
+      mutate(dur_first_mean = na_if(dur_first_mean, NaN))
+    # ready to plot!
+  }
+  
+  if (case == 2){
+    Fsim <- Fsim %>% 
+      filter(first,
+             !is.na(dis_dur)) %>% 
+      group_by(age) %>% 
+      summarize(order_first_mean = mean(dis_order, na.rm = TRUE)) %>% 
+      mutate(order_first_mean = na_if(order_first_mean, NaN))
+  }
+  
+  if (case == 3){
+    Fsim <- Fsim %>%
+      filter(dead) %>% 
+      group_by(id) %>% 
+      mutate(ttd = max(age) - age,
+             ad5 = max(age) - max(age) %% 5) %>% 
+      ungroup() %>% 
+      filter(ad5 > 30) %>% 
+      group_by(ttd, ad5) %>% 
+      summarize(ttdprev = mean(state == "Disabled"))
+  }
+  Fsim
+}
+
+
+
+
